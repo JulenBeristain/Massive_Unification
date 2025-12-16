c10bf143e3d18ea6ae5ba48d64e1c317b66e066d
Julen Beristain <julberistain@gmail.com>
2025-12-16 13:02:59 +0100

    Schema from term and variables, size of schemas and start with set of repeated variables of terms

--- Schemas/schemas.c
+++ Schemas/schemas.c
@@ -1,13 +1,90 @@
-#include "Schemas/schemas.h"
+#include "schemas.h"
+#include <stdlib.h>
 
-bool isVariable(Schema s) { return s.v > 0; }
-bool isEmpty(Schema s) { return s.v == 0 && s.arity == 0; }
+/**
+ * NOTE: if Schemas are modified at some point, so they are not immutable, then the use of the
+ *       size attribute must be reconsidered.
+ */
 
-void initSchema(Schema *s, unsigned arity) {
+/**
+ * This is called in the general case, not variable or empty.
+ * 
+ * Therefore, size is initialized to 1 already, then the sizes of the subschemas
+ * are to be added.
+ */
+void init_nested_schema(Schema *s, unsigned arity) {
     s->v = 0;
     s->arity = arity;
     if (arity) {
         s->subschemas = malloc(arity * sizeof(*(s->subschemas)));
     }
+    s->size = 1;
 }
 
+Schema *schema_from_term_and_variables(Term *term, SetVariables set_variables){
+    Schema *schema = malloc(sizeof(*schema));
+    if(schema == NULL){
+        perror("malloc failed to allocate memory");
+        exit(EXIT_FAILURE);
+    }
+
+    if(is_variable(term)){
+        if(lookup_set(set_variables, term->v)){
+            schema->v = term->v;
+        } else {
+            schema->v = 0;
+            schema->arity = 0;
+        }
+        schema->size = 1;
+    } else {
+        init_nested_schema(schema, term->arity);
+        Schema *subschema = schema->subschemas;
+        Term *subterm = term->subterms;
+        for(uint32_t i = term->arity; i; --i, ++subschema, ++subterm){
+            *subschema = *schema_from_term_and_variables(term, set_variables);
+            schema->size += subschema->size;
+        }
+    }
+    return schema;
+}
+
+/**
+ * Function that computes the schema size, without looking at the precached sizes.
+ */
+unsigned schema_size(Schema *s){
+    if(is_schema_variable(s) || is_schema_empty(s)){ return 1; }
+    unsigned total_size = 1;
+    Schema *subschema = s->subschemas;
+    for(unsigned i = s->arity; i; --i, ++subschema){
+        total_size += schema_size(subschema);
+    }
+    return total_size;
+}
+
+
+/**
+ * NOTE: pointers to repeated_vars and vars are perfectly valid candidates for
+ *       the term struct.
+ */
+SetVariables repeated_vars_in_term(Term *t){
+    SetVariables vars = create_set_defsize();
+    SetVariables repeated_vars = create_set_defsize();
+    repeated_vars_in_term_(t, &vars, &repeated_vars);
+    return repeated_vars;
+}
+
+static void repeated_vars_in_term_(Term *t, SetVariables *vars, SetVariables *repeated_vars){
+    if(is_term_variable(t)){
+        if(lookup_set(*vars, t->v)){
+            insert_to_set(repeated_vars, t->v);
+        } else {
+            insert_to_set(vars, t->v);
+        }
+    }
+
+    if(is_term_empty(t)){
+
+    }
+
+    //General case
+}
--- Schemas/schemas.h
+++ Schemas/schemas.h
@@ -2,7 +2,8 @@
 #define SCHEMAS_H
 
 #include <stdbool.h>
-#include "Schemas/set.h"
+#include <stddef.h>
+#include "set_variables.h"
 
 /**
  * The structure that represents a Schema. It is inductively defined. It can be:
@@ -16,31 +17,44 @@
  * In this version, we only use 16B of memory for each Schema, just the space of
  * two pointers (in x64 modern architectures) the subschemas are continuously 
  * allocated in memory.
+ * 
  */
 typedef struct Schema Schema;
 struct Schema
 {
     unsigned v;         // v > 0, variable | v = 0, general case
     unsigned arity;     // if isVariable, ignore. Else, number of sub-schemas (could be 0 for <>)
-    // NOTE: see if an extra indirection (**subschemas) is necessary/helpful.
+    // TODO: see if an extra indirection (**subschemas) is necessary/helpful.
     Schema *subschemas; // if isVariable, ignore. Else if arity = 0, ignore. Else, the sub-schemas in a compact array
+    size_t size;        // Note, candidate for uint32_t if padding may arise | 
+                            //TODO: might be interesting to define a Schema wrapper if we are only interested in the 
+                            //      sizes of the outermost Schemas, so not every sub-schema stores its size.
 };
 
-bool isVariable(Schema schema);
-bool isEmpty(Schema schema);
-void initSchema(Schema *schema, unsigned arity);
-
-// NOTE: we are going to parse some file to term structs like this? Or are we going to directly parse terms into
+// TODO: we are going to parse some file to term structs like this? Or are we going to directly parse terms into
 //  Schemas? The latter is strange, we should have the terms we are working with somehow in memory...
+/**
+ * Very similar to Schemas, but with the extra main symbol.
+ */
 typedef struct Term Term;
 struct Term
 {
-    unsigned v;             // v > 0, variable | v = 0, function (either constant or with subterms)
     unsigned mainSymbol;
-    unsigned arity;     
+    unsigned size;          // See Schemas comment...
+    unsigned v;             // v > 0, variable | v = 0, function (either constant or with subterms)
+    unsigned arity;
     Term *subterms;
 };
 
-Schema *schemaFromTermAndVariables(Term term, Set setVariables);
+static inline bool is_schema_variable(Schema *s) { return s->v > 0; }
+static inline bool is_schema_empty(Schema *s) { return s->v == 0 && s->arity == 0; }
+static inline bool is_term_variable(Term *t) { return t->v > 0; }
+static inline bool is_term_empty(Term *t) { return t->v == 0 && t->arity == 0; }
+
+void init_nested_schema(Schema *schema, unsigned arity);
+Schema *schema_from_term_and_variables(Term *term, SetVariables set_variables);
+unsigned schema_size(Schema *s);
+
+SetVariables repeated_vars_in_term(Term *t);
 
 #endif //SCHEMAS_H
